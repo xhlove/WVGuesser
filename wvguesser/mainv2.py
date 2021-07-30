eaaa7ed2e67face13bec5f7e5435b938c70cfdb9
NanDesuKa-FR <74000964+NanDesuKa-FR@users.noreply.github.com>
2021-07-31 01:00:50 +0200

    Add files via upload

--- wvguesser/mainv2.py
+++ wvguesser/mainv2.py
@@ -6,44 +6,82 @@ import signal
 import socket
 import binascii
 import subprocess
+from typing import List
+from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 from random import randint
 from Crypto.Cipher import AES
 from Crypto.Hash import CMAC
 from notifypy import Notify
 import ctypes
+import socket
 
 
-port = randint(20000, 50000)
-MAIN_EXE = (Path('.') / 'main.exe').resolve().as_posix()
-p = subprocess.Popen(f'{MAIN_EXE} {port}', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-time.sleep(1)
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', port))
-
-notify_me = True
 ctypes.windll.kernel32.SetConsoleTitleW("WVGuesser v1.1.0")
 
+with open("WVGuesserConf.json") as json_data:
+    config = json.load(json_data)
 
-def handle_exit():
-    p.kill()
+thread_conf = int(config['thread'])
+notif_conf = str(config['notif'])
 
 
-def call_func(msg: bytes):
-    client.send(msg.encode("utf-8"))
+def find_free_port():
+    s = socket.socket()
+    s.bind(('', 0))
+    return s.getsockname()[1]
+
+
+servers = []
+clients = []
+for i in range(5):
+    port = find_free_port()
+
+    MAIN_EXE = (Path('.') / 'main.exe').resolve().as_posix()
+    p = subprocess.Popen(f'{MAIN_EXE} {port}', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    servers.append(p)
+    time.sleep(0.3)
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('127.0.0.1', port))
+    clients.append(client)
+
+
+def handle_exit(signum, frame):
+    close()
+
+
+def close():
+    try:
+        [p.close() for p in clients]
+        [p.kill() for p in servers]
+    except Exception:
+        pass
+
+
+def call_func(client, msg: bytes):
+    client.send(msg)
     resp = client.recv(1024)
     return resp.decode('utf-8').strip()
 
 
-def guessInput(text: str):
-    return call_func(f'guessInput|{text}\n')
+def multi_guessInput(bufs: List[bytes]):
+    results = []
+    with ThreadPoolExecutor(max_workers=thread_conf) as executor:
+        results = executor.map(guessInput, clients, bufs)
+    return results
 
 
-def getDeoaep(text: str):
-    return call_func(f'getDeoaep|{text}\n')
+def guessInput(client, buf: bytes):
+    # return call_func(client, f'guessInput|{buf}\n')
+    return call_func(client, b'guessInput|' + buf + b'\n')
 
 
-def run(hex_session_key: str):
+def getDeoaep(client, buf: bytes):
+    # return call_func(client, f'getDeoaep|{buf}\n')
+    return call_func(client, b'getDeoaep|' + buf + b'\n')
+
+
+def runv2(hex_session_key: str):
     ts = time.time()
     encKey = binascii.a2b_hex(hex_session_key)
     #print(hex_session_key)
@@ -55,22 +93,19 @@ def run(hex_session_key: str):
         offs = math.floor((offset - 2) % 4)
         desired = (encKey[len(encKey) - bt - 1] >> (offs * 2)) & 3
         destail = hex_session_key[len(hex_session_key) - bt * 2:len(hex_session_key)]
+        bufs = []
         j = buf[offset]
-        while j < 8:
-            buf[offset] = j
-            st = binascii.b2a_hex(bytes(buf)).decode('utf-8')
-            # print(st)
-            val = guessInput(st)
-            # print(val)
+        for _j in range(5):
+            buf[offset] = _j
+            bufs.append(binascii.b2a_hex(bytes(buf)))
+        for _j, val in enumerate(multi_guessInput(bufs)):
             sub = int(val[len(val) - bt * 2 - 2:len(val) - bt * 2], 16)
             got = (sub >> (offs * 2)) & 3
             gtail = val[len(hex_session_key) - bt * 2:len(hex_session_key) + bt * 2]
             if got == desired and gtail == destail:
-                # if offset % 16 == 2:
-                #     print(val)
-                #     pass
+                buf[offset] = _j
+                j = buf[offset]
                 break
-            j += 1
         if j == 8:
             buf[offset] = 0
             offset -= 1
@@ -88,14 +123,12 @@ def run(hex_session_key: str):
         else:
             offset += 1
     # print(f'==> Elapsed time {time.time() - ts:.2f}s')
+    ctypes.windll.kernel32.SetConsoleTitleW(f'WVGuesser v1.1.0 - Elapsed time {time.time() - ts:.2f}s')
     # print("Output", buf)
-    st = binascii.b2a_hex(bytes(buf)).decode('utf-8')
-    outp = getDeoaep(st)
+    outp = getDeoaep(clients[0], binascii.b2a_hex(bytes(buf)))
     # print(outp)
     if len(outp) < 10:
         assert 1 == 0, 'Could not remove padding, probably invalid key'
-    # print(st)
-    p.kill()
     return outp
 
 
@@ -118,7 +151,7 @@ def decrypt_license_keys(session_key: str, context_enc: str, key_infos: dict):
         json_key = json.dumps(list_key)
         print(json_key)
 
-        if notify_me:
+        if notif_conf == "True":
             notification = Notify()
             notification.title = "WVGuesser"
             notification.message = f"{len(list_key)} keys found."
@@ -134,9 +167,10 @@ def main():
     if len(sys.argv) == 2:
         path = sys.argv[1]
     else:
-        path = (Path('.') / 'offline_config_yk.json').resolve().as_posix()
+        path = (Path('.') / 'offline_config.json').resolve().as_posix()
     config = json.loads(Path(path).read_text(encoding='utf-8'))
-    clear_session_key = run(config['enc_session_key'])
+    clear_session_key = runv2(config['enc_session_key'])
+    close()
     decrypt_license_keys(clear_session_key, config['enc_key'], config['key_infos'])
     sys.stdin.read()
 
