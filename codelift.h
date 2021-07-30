f4947b1ff781f66ea64ee89707810e3b1a948c31
NanDesuKa-FR <74000964+NanDesuKa-FR@users.noreply.github.com>
2021-07-30 19:20:50 +0200

    Add files via upload

---
+++ codelift.h
@@ -0,0 +1,7 @@
+#include <stdint.h>
+extern "C" {
+    const char* guessInput(const char* input);
+    const char* getOutput(const char* input);
+    const char* getDeoaep(const char* input);
+    void freeStr(void* str);
+}
