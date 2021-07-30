afef88916bc519eeede6a6006ac295dcd3d3c0fd
NanDesuKa-FR <74000964+NanDesuKa-FR@users.noreply.github.com>
2021-07-30 19:23:32 +0200

    Delete codelift.h

--- codelift.h
+++
@@ -1,7 +0,0 @@
-#include <stdint.h>
-extern "C" {
-    const char* guessInput(const char* input);
-    const char* getOutput(const char* input);
-    const char* getDeoaep(const char* input);
-    void freeStr(void* str);
-}
